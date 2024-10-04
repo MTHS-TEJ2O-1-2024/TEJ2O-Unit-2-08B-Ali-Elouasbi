630ebea3e21f2569c2d5514191346f66e7706199
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-10-04 10:51:36 -0400

    added spaces

--- main.ts
+++ main.ts
@@ -11,21 +11,25 @@ basic.showIcon(IconNames.Happy)
 // turn on LED
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
+    
     //pin13 = Red (1,0,0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString("Red")
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
+    
     //pin14 = Green (0,1,0)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString("Green")
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
+    
     //pin15 = Blue (0,0,1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString("Blue")
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
+    
     //Magenta (1,0,1) = pin13 and pin15
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
@@ -33,6 +37,7 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
+    
     //Yellow (1,1,0) = pin13 and pin14
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
@@ -40,6 +45,7 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
+    
     //Cyan (0,1,1) = pin14 and pin15
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
@@ -47,6 +53,7 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
+    
     //White (1,1,1) = pin13, pin14 and pin15
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
